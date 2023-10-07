8371dfc5475df530570c9552d8bfaebea06e836d
OVP2023 <138027862+OVP2023@users.noreply.github.com>
2023-10-08 01:13:32 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,53 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
 
-function CardView(props) {
-  const {product} = props;
-  return (
-    <div>
-      <img
-        className="CardView"
-        src={product.img}
-      /> 
-      <div className="Name">{product.name}</div>
-      <div className="Price">${product.price}</div>
-      <div className="Color">{product.color}</div>
-    </div>
-  );  
+class CardView extends Component{
+  constructor(props) {
+    super(props);
+  } 
+  render() { 
+    return (
+      <div key={"df"+`${this.props.product.id}`}>
+        <img
+          key={"i"+`${this.props.product.id}`}
+          className="CardView"
+          src={this.props.product.img}
+        /> 
+        <div   key={"da"+`${this.props.product.id}`} className="Name">{this.props.product.name}</div>
+        <div key={"db"+`${this.props.product.id}`} className="Price">${this.props.product.price}</div>
+        <div key={"dc"+`${this.props.product.id}`} className="Color">{this.props.product.color}</div>
+      </div>
+    );
+  }  
 }
 
-function ListView(props) {
-  const {product} = props;
-  return (
-    <div>
-      <div key={product.name}  className="List"> {product.name } ,  $ {product.price} ,    {product.color}</div>
-    </div>
-  );  
+class ListView extends Component{
+  constructor(props) {
+    super(props);
+  } 
+
+  render(){
+    return (
+      <div key={"dl"+`${this.props.product.id}`}>
+        <div  key={"de"+`${this.props.product.id}`} className="List"> {this.props.product.name } ,  $ {this.props.product.price} ,    {this.props.product.color}</div>
+      </div>
+    );
+    }  
 }
 
-function IconSwitch(props) {
-  return (
-    <div>
-      <button onClick={props.onSwitch}>
-        {props.icon}
-      </button>
-    </div>
-  );
+class IconSwitch extends Component{
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return (
+      <div>
+        <button  onClick={this.props.onSwitch}>
+          {this.props.icon}
+        </button>
+      </div>
+    );
+  }
 }
 
 class Store extends Component{
@@ -43,31 +58,37 @@ class Store extends Component{
     this.state = {isToggleOn: true}
     
     this.products = [{
+      id:"1",
       name: "Nike Metcon 2",
       price: "130",
       color: "red",
       img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/1.jpg"
       }, {
+      id:"2",
       name: "Nike Metcon 2",
       price: "130",
       color: "green",
       img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/2.jpg"
       }, {
+      id:"3",
       name: "Nike Metcon 2",
       price: "130",
       color: "blue",
       img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/3.jpg"
     }, {
+      id:"4",
       name: "Nike Metcon 2",
       price: "130",
       color: "black",
       img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/4.jpg"
     }, {
+      id:"5",
       name: "Nike free run",
       price: "170",
       color: "black",
       img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/7.jpg"
     }, {
+      id:"6",
       name: "Nike Metcon 3",
       price: "150",
       color: "green",
